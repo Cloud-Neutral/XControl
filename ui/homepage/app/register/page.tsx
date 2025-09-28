be8dc1fc80262115570e21689a49fe86271ff409
shenlan <manbuzhe2009@qq.com>
2025-09-28 23:22:15 +0800

    Revert "Add social auth ready login and registration pages (#317)"
    This reverts commit 90f573fd45f04f100ca07f35c1e388745f854cb8.

--- ui/homepage/app/register/page.tsx
+++ ui/homepage/app/register/page.tsx
@@ -1,15 +1,13 @@
 export const dynamic = 'error'
 
-import { notFound } from 'next/navigation'
+import { notFound, redirect } from 'next/navigation'
 
 import { isFeatureEnabled } from '@lib/featureToggles'
-
-import RegisterContent from './RegisterContent'
 
 export default function RegisterPage() {
   if (!isFeatureEnabled('globalNavigation', '/register')) {
     notFound()
   }
 
-  return <RegisterContent />
+  redirect('/panel/ldp/users')
 }
